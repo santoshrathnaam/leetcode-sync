8649fc789e3cd6f9c0425f48989afadaa67a5c37
Santosh Rathnaam <rathnaamsantosh@gmail.com>
2026-09-07 22:47:58 +0530

    feat: add LeetCode #11 TwoPointer-(Container with most water)

---
+++ Arrays/Medium/0011-TwoPointer-(Container-with-most-water).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int Maxwater=0;
+        while(left<right){
+                int h=Math.min(height[left],height[right]);
+                int width=right-left;
+                Maxwater=Math.max(Maxwater,h*width);
+                if(height[left]>height[right]){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+        }
+        return Maxwater;
+    }
+}
