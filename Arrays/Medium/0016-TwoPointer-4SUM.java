cb448de4b21e77a3efebccb2ed71971dcc590980
Santosh Rathnaam <rathnaamsantosh@gmail.com>
2026-09-08 19:29:24 +0530

    update: improve LeetCode #16 TwoPointer-4SUM

--- Arrays/Medium/0016-TwoPointer-4SUM.java
+++ Arrays/Medium/0016-TwoPointer-4SUM.java
@@ -1,9 +1,4 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-        int resultsum=nums[0]+nums[1]+nums[2];
-        int mindiff=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length-2;i++){class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res =new ArrayList<>();
         Arrays.sort(nums);
@@ -46,29 +41,5 @@ class Solution {
             
         }
         return res;
-    }
-}
-            int left=i+1;
-            int right=nums.length-1;
-            
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if (sum==target){
-                    return target;
-                }
-                if(sum<target){
-                    left++;
-                }
-                else{
-                    right--;
-                }
-                int diff=Math.abs(sum-target);
-                if(diff<mindiff){
-                    mindiff=diff;
-                    resultsum=sum;
-                }
-            }
-        }  
-        return resultsum;
     }
 }
