7cb40b98defe878cfeb5d6d168b71b1b5e3a949b
Santosh Rathnaam <rathnaamsantosh@gmail.com>
2026-09-08 18:57:21 +0530

    feat: add LeetCode #16 TwoPointer-3SUM CLosest

---
+++ Arrays/Hard/0016-TwoPointer-3SUM-CLosest.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int resultsum=nums[0]+nums[1]+nums[2];
+        int mindiff=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+            
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if (sum==target){
+                    return target;
+                }
+                if(sum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+                int diff=Math.abs(sum-target);
+                if(diff<mindiff){
+                    mindiff=diff;
+                    resultsum=sum;
+                }
+            }
+        }  
+        return resultsum;
+    }
+}
